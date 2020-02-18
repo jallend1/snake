5643e7b3193c635b51de7cd02a9306e40e9800ee
jallend1 <jallend1@gmail.com>
2020-02-17 20:19:36 -0800

    Added borders and basic game over function

--- app.js
+++ app.js
@@ -7,10 +7,39 @@ const widthInBlocks = width / blockSize;
 const heightInBlocks = height / blockSize;
 let score = 0;
 
-const drawBorder = function (){
+function drawBorder(){
     ctx.fillStyle = "Gray";
     ctx.fillRect(0, 0, width, blockSize);
     ctx.fillRect(0, height - blockSize, width, blockSize);
     ctx.fillRect(0, 0, blockSize, height);
     ctx.fillRect(width - blockSize, 0, blockSize, height);
 }
+
+function drawScore(){
+    ctx.font = '20px Courier';
+    ctx.fillStyle = 'Black';
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'top';
+    ctx.fillText(`Score: ${score, blockSize, blockSize}`, 50, 50);
+}
+
+function gameOver(){
+    clearInterval(intervalID);
+    ctx.font = '60px Courier';
+    ctx.fillStyle = 'Black';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText('Game Over', width / 2, height / 2);
+}
+
+const intervalid = setInterval(function (){
+    ctx.clearRect(0, 0, width, height);
+    drawScore();
+    snake.move();
+    snake.draw();
+    apple.draw();
+    drawBorder();
+}, 100);
+
+drawBorder();
+drawScore();
